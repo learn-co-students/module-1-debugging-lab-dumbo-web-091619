5f03038a7d72712628b3fde3c22d5de5abf8a877
Ali Shah <ali1195shah@gmail.com>
2019-10-06 21:06:43 +0000

    Done.

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -1,17 +1,21 @@
-
+require 'pry'
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
 end
 
 def turtle_traits(turtles)
+  new_arr = []
   turtles.map do |turtle|
-    turtle[:traits].each do |trait|
-      trait
-    end
+    # binding.pry
+    new_arr << turtle[:traits]
+    # binding.pry
   end
+  new_arr
+  # binding.pry
+  # # 0
 end
