10312c05516178dbbde5ef6c3e3c8fdc88a678c4
Reina <r.clarkemitchell@gmail.com>
2019-10-06 01:08:42 -0400

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,12 @@
+require 'pry'
+
 def snake_it_up(string)
   if string[0] == "s"
-  10 * "s" + string
+  new_string = ""
+  multi = 10.times {new_string += "s"}
+  new_string + string
   else
   string
   end
 end
+
